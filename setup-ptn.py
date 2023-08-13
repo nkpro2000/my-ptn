78b042dcd40a38d47076cdefbe47080fc8fd60a0
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2023-08-13 18:35:50 +0530

    script to Setup /ptn/
    ```
    sh> tree -puag -I .git /ptn
    [drwxr-xr-x root     root    ]  /ptn
    ├── [drwxr-xr-x root     root    ]  1
    ├── [drwxr-xr-x root     root    ]  b
    │   ├── [lrwxrwxrwx root     root    ]  0001 -> ../s/liveo
    │   ├── [lrwxrwxrwx root     root    ]  0010 -> ../s/vtesp
    │   ├── [lrwxrwxrwx root     root    ]  0011 -> ../s/efisp
    │   ├── [lrwxrwxrwx root     root    ]  0100 -> ../s/winos
    │   ├── [lrwxrwxrwx root     root    ]  0101 -> ../s/winrp
    │   ├── [lrwxrwxrwx root     root    ]  0110 -> ../s/winsp
    │   ├── [lrwxrwxrwx root     root    ]  0111 -> ../s/mswrp
    │   ├── [lrwxrwxrwx root     root    ]  1000 -> ../s/linux
    │   ├── [lrwxrwxrwx root     root    ]  1001 -> ../s/lbuvm
    │   └── [lrwxrwxrwx root     root    ]  1011 -> ../s/linex
    ├── [drwxr-xr-x root     root    ]  d
    │   ├── [lrwxrwxrwx root     root    ]  1 -> ../s/liveo
    │   ├── [lrwxrwxrwx root     root    ]  11 -> ../s/linex
    │   ├── [lrwxrwxrwx root     root    ]  2 -> ../s/vtesp
    │   ├── [lrwxrwxrwx root     root    ]  3 -> ../s/efisp
    │   ├── [lrwxrwxrwx root     root    ]  4 -> ../s/winos
    │   ├── [lrwxrwxrwx root     root    ]  5 -> ../s/winrp
    │   ├── [lrwxrwxrwx root     root    ]  6 -> ../s/winsp
    │   ├── [lrwxrwxrwx root     root    ]  7 -> ../s/mswrp
    │   ├── [lrwxrwxrwx root     root    ]  8 -> ../s/linux
    │   └── [lrwxrwxrwx root     root    ]  9 -> ../s/lbuvm
    ├── [drwxr-xr-x root     root    ]  l
    │   └── [drwxr-xr-x root     root    ]  bak
    ├── [drwx------ nkpro    nkpro   ]  .nkpro
    │   └── [drwxr-xr-x nkpro    nkpro   ]  PtnTable_git
    │       └── [-rw-r--r-- nkpro    nkpro   ]  README.md
    ├── [drwxr-xr-x root     root    ]  r
    │   ├── [lrwxrwxrwx root     root    ]  EFISP -> ../s/efisp
    │   ├── [lrwxrwxrwx root     root    ]  LBuVM -> ../s/lbuvm
    │   ├── [lrwxrwxrwx root     root    ]  LinEx -> ../s/linex
    │   ├── [lrwxrwxrwx root     root    ]  Linux -> ../s/linux
    │   ├── [lrwxrwxrwx root     root    ]  LiveO -> ../s/liveo
    │   ├── [lrwxrwxrwx root     root    ]  MSwRP -> ../s/mswrp
    │   ├── [lrwxrwxrwx root     root    ]  VTESP -> ../s/vtesp
    │   ├── [lrwxrwxrwx root     root    ]  WinOS -> ../s/winos
    │   ├── [lrwxrwxrwx root     root    ]  WinRP -> ../s/winrp
    │   └── [lrwxrwxrwx root     root    ]  WinSP -> ../s/winsp
    ├── [drwxr-xr-x root     root    ]  s
    │   ├── [lrwxrwxrwx root     root    ]  efisp -> /dev/disk/by-label/EFISP
    │   ├── [lrwxrwxrwx root     root    ]  lbuvm -> /dev/disk/by-label/LBuVM
    │   ├── [lrwxrwxrwx root     root    ]  linex -> /dev/disk/by-label/LinEx
    │   ├── [lrwxrwxrwx root     root    ]  linux -> /dev/disk/by-label/Linux
    │   ├── [lrwxrwxrwx root     root    ]  liveo -> /dev/disk/by-label/LiveO
    │   ├── [lrwxrwxrwx root     root    ]  mswrp -> /dev/disk/by-label/MSwRP
    │   ├── [lrwxrwxrwx root     root    ]  vtesp -> /dev/disk/by-label/VTESP
    │   ├── [lrwxrwxrwx root     root    ]  winos -> /dev/disk/by-label/WinOS
    │   ├── [lrwxrwxrwx root     root    ]  winrp -> /dev/disk/by-label/WinRP
    │   └── [lrwxrwxrwx root     root    ]  winsp -> /dev/disk/by-label/WinSP
    ├── [drwxr-xr-x root     root    ]  v
    │   ├── [drwxr-xr-x root     root    ]  esp
    │   └── [drwxr-xr-x root     root    ]  main
    └── [drwxr-xr-x root     root    ]  w
        ├── [drwxr-xr-x root     root    ]  esp
        └── [drwxr-xr-x root     root    ]  main
    
    16 directories, 41 files
    ```

---
+++ setup-ptn.py
@@ -0,0 +1,61 @@
+#TODO add shebang with proper python env.
+# This is the first file to run after cloning so that all are configured as intended.
+# See comments ######### for steps involved in this process done by this script.
+# Will be run by user like `python setup-ptn.py`, after cloning this repo (follow README.md).
+
+import os
+
+PTN = '/ptn/'
+PTN_GIT = PTN+ '.'+os.environ['USER'] +'/PtnTable_git/'
+
+
+# Making Dirs and Symlinking
+#############################
+#/ptn/
+##├── b (binary)  /0001  /0100  /1001  ...
+##├── d (decimal) /1     /4     /9     ...
+##├── r (Label)   /LiveO /WinOS /LBuVM ...
+##├── s (label)   /liveo /winos /lbuvm ...
+##├── 1 (first partition)
+##├── l (linux)   /bak
+##├── w (windows) /main  /esp
+##└── v (ventoy)  /main  /esp
+TREE = (
+    ( 1, 'LiveO', 'drb'),
+    ( 2, 'VTESP', 'drb'),
+    ( 3, 'EFISP', 'drb'),
+    ( 4, 'WinOS', 'drb'),
+    ( 5, 'WinRP', 'drb'),
+    ( 6, 'WinSP', 'drb'),
+    ( 7, 'MSwRP', 'drb'),
+    ( 8, 'Linux', 'drb'),
+    ( 9, 'LBuVM', 'drb'),
+    (11, 'LinEx', 'drb'),
+)
+ops = []
+for i in 'bdrs':
+    ops.append(f'mkdir -p {PTN}{i}')
+for d, r, dirs in TREE:
+    b = bin(d)[2:].zfill(4)
+    s = r.lower()
+    ops.append(f'ln -sf /dev/disk/by-label/{r} {PTN}s/{s}')
+    for i in dirs:
+        ops.append(f'ln -sf ../s/{s} {PTN}{i}/{locals()[i]}')
+for i in 'lwv':
+    ops.append(f'mkdir -p {PTN}{i}')
+for i in ['1', 'l/bak', 'w/main', 'w/esp', 'v/main', 'v/esp']:
+    ops.append(f'mkdir -p {PTN}{i}')
+
+script = r'''# shellcheck shell=sh
+
+rm -rf /ptn/*
+'''
+script+= '\n'.join(ops)
+
+with open(PTN_GIT+'ops-todo.sh', 'w') as f:
+    f.write(script)
+print('I> To run bash Script to create the dir/link tree')
+print('$> sh> sudo bash '+ PTN_GIT+'ops-todo.sh # Ctrl+D to skip')
+os.system('sudo bash '+ PTN_GIT+'ops-todo.sh')
+os.system('rm '+ PTN_GIT+'ops-todo.sh')
+os.system('sudo -K')
